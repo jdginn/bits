313f3f4e9c8e7fb2e5f84dfe9327a1127ec44d23
Justin Ginn <Justindavid.ginn@gmail.com>
2021-11-08 17:10:34 -0600

    Baby's first function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use clap::{App, Arg};
 
+fn get_bitvector(hex_string: &str) -> Vec<u32> {
+    let mut bits = Vec::<u32>::with_capacity(hex_string.len() * 4);
+    for c in hex_string.chars() {
+        println!("{:?}", c);
+        let digit = c.to_digit(16).unwrap();
+        bits.push(digit >> 3 & 0b1);
+        bits.push(digit >> 2 & 0b1);
+        bits.push(digit >> 1 & 0b1);
+        bits.push(digit & 0b1);
+    println!("{:?}", bits);
+    }
+    return bits
+}
+
 fn main() {
     let matches = App::new("bits")
         .version("1.0")
@@ -13,14 +27,5 @@ fn main() {
         .get_matches();
 
     let hex_string = matches.value_of("hex_string").unwrap();
-    let mut bits = Vec::with_capacity(hex_string.len() * 4);
-    for c in hex_string.chars() {
-        println!("{:?}", c);
-        let digit = c.to_digit(16).unwrap();
-        bits.push(digit >> 3 & 0b1);
-        bits.push(digit >> 2 & 0b1);
-        bits.push(digit >> 1 & 0b1);
-        bits.push(digit & 0b1);
-    println!("{:?}", bits);
-    }
+    get_bitvector(hex_string);
 }
