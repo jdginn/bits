2eb7d38deb148a5ca69a6d030cf9dc911d4fbd9c
Justin Ginn <Justindavid.ginn@gmail.com>
2021-11-08 19:27:18 -0600

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{App, Arg};
-use std::io::{Write};
+use std::io::Write;
 
 fn get_bitvector(hex_string: &str) -> Vec<u32> {
     let mut bits = Vec::<u32>::with_capacity(hex_string.len() * 4);
@@ -10,25 +10,30 @@ fn get_bitvector(hex_string: &str) -> Vec<u32> {
         bits.push(digit >> 1 & 0b1);
         bits.push(digit & 0b1);
     }
-    return bits
+    return bits;
 }
 
 fn print_bits(hex_string: &str) {
-   let bits = get_bitvector(hex_string);
-   let mut rows = vec!["".to_string(), "".to_string(), "".to_string(), "".to_string(), "".to_string()];
-   // let s: String = "".to_string();
-   for i in 0..bits.len() {
+    let bits = get_bitvector(hex_string);
+    let mut rows = vec![
+        "".to_string(),
+        "".to_string(),
+        "".to_string(),
+        "".to_string(),
+        "".to_string(),
+    ];
+    for i in 0..bits.len() {
         if i % 4 == 0 {
             for j in 0..rows.len() - 1 {
                 rows[j].push_str(" ")
             }
             rows[4].push_str("    ");
-            rows[4].push_str(&hex_string[i / 4 .. i / 4 + 1].to_uppercase());
+            rows[4].push_str(&hex_string[i / 4..i / 4 + 1].to_uppercase());
         }
 
         let s = format!("{:0>2}", i.to_string());
         for j in 0..s.len() {
-            rows[j].push_str(&s[j..j+1])
+            rows[j].push_str(&s[j..j + 1])
         }
         rows[2].push_str("");
         rows[3].push_str(&bits[i].to_string());
@@ -39,7 +44,6 @@ fn print_bits(hex_string: &str) {
 
     for row in rows.iter() {
         handle.write_all(format!("{:}\n", row).as_bytes()).unwrap();
-        // println!("{}", row)
     }
 }
 
